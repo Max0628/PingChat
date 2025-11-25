23f4b48fc644e75af1636bc10c88ecf5490df5eb
Maxchauo0628 <maxchauo0628@gmail.com>
2025-11-26 06:40:04 +0800

    [feat] add backend project.

---
+++ apps/backend/src/main/java/com/pingchat/pingchat_backend/PingChatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pingchat.pingchat_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingChatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingChatBackendApplication.class, args);
+	}
+
+}
---
+++ apps/backend/src/test/java/com/pingchat/pingchat_backend/PingChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pingchat.pingchat_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingChatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
