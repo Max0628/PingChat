88844912d3169ddf615de2e0983288f54e2afdd2
Maxchauo0628 <maxchauo0628@gmail.com>
2025-11-29 14:38:39 +0800

    [chore] add config.

---
+++ apps/frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+import { defineConfig } from 'tailwindcss'
+
+export default defineConfig({
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx,js,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+})
